5bfa2280b10e68fd2a33f235ba13c02a69446e50
Sammie156 <samanwayadatta@gmail.com>
2021-07-13 15:26:49 +0530

    Initialized BufferedReader for reading Input from the User �

--- src/Program1.java
+++ src/Program1.java
@@ -1,8 +1,13 @@
 import java.io.*;
 
-class Program1{
+class Program1 {
   public static void main(String[] args) throws IOException {
-    //Using DataInputStream for handling input from the user.
-    DataInputStream ds = new DataInputStream(System.in);
+    // Using DataInputStream for handling input from the user.
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Enter your name : ");
+    String name = br.readLine();
+
+    System.out.println(name);
   }
 }
