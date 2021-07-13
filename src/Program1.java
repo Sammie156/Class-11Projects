a6b0d2a62a8c248768c13401171b0d72ebd708f3
Sammie156 <samanwayadatta@gmail.com>
2021-07-13 19:05:11 +0530

    Started Working on the First Program

--- src/Program1.java
+++ src/Program1.java
@@ -5,9 +5,18 @@ class Program1 {
     // Using DataInputStream for handling input from the user.
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    System.out.println("Enter your name : ");
-    String name = br.readLine();
+    // Integer to hold Number of students
+    int n = 0;
 
-    System.out.println(name);
-  }
+    System.out.println("Enter number of students : ");
+    n = Integer.parseInt(br.readLine());
+
+    int rows = n; // Number of rows of the DDA
+    int columns = 5; // Number of columns of the DDA
+
+    int[][] marks = new int[rows][columns]; // Creating the DDA for storing the marks of 'n' number of students.
+    String[] names = new String[n]; // Creating a Single-Dimension Array to hold the names of the students.
+    char[] grade = new char[n]; // A Single-Dimensional Array to hold the grade of the students.
+    double[] total = new double[n]; // A Single-Dimensional Array to hold the total of the students.
+  
 }
