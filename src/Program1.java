b63ed07f506cd019b3f88c2441a5fd7e8402febe
Sammie156 <samanwayadatta@gmail.com>
2021-07-13 09:17:20 +0530

    Started working on the first Program ⚽

--- src/Program1.java
+++ src/Program1.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 class Program1{
   public static void main(String[] args) throws IOException {
+    //Using DataInputStream for handling input from the user.
     DataInputStream ds = new DataInputStream(System.in);
   }
 }
